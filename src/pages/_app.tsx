4ef1d54eaa8d616898bf178d109d1504f4c18618
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2023-08-20 12:37:07 -0700

    add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,24 @@ const MyApp: AppType = ({ Component, pageProps }) => {
       <Head>
         <title>breadnime! - ad free anime</title>
         <meta name="description" content="Totally ad free anime" />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <main className="container space-y-1">
         <Component {...pageProps} />
