343ec2dbe89941cd9567d1e7f301d14c7cd96af2
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2023-08-19 20:43:15 -0700

    fix path

--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
-import Link from "next/link";
 import { useRouter } from "next/router";
 
 interface Props {
