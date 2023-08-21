38597fd1d4b5f8dbde322080643db36c4a6d8d63
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2023-08-20 20:23:04 -0700

    add proper 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,12 @@
+import { Navbar } from "~/components/navbar";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <h1 className="py-32 text-center text-6xl font-bold text-primary md:py-96">
+        404 Not Found.
+      </h1>
+    </>
+  );
+}
